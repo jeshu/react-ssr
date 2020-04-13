7efd19cf72194931594bb4a32737c43db0898413
Brijesh Kumar <jeshu911@gmail.com>
2020-04-14 00:07:07 +0530

    Port updated

--- src/index.js
+++ src/index.js
@@ -45,6 +45,6 @@ app.get("*", (req, res) => {
   Promise.all(promises).then(render);
 });
 
-app.listen(3000, () => {
+app.listen(( process.env.PORT || 3000), () => {
   console.log("server running on port 3000");
 });
