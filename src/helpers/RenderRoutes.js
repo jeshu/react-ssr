616f62d26b24a2862811202a86b63552044a8035
Brijesh Kumar <jeshu911@gmail.com>
2020-04-14 00:00:41 +0530

    render route issue fixed

--- src/helpers/RenderRoutes.js
+++ src/helpers/RenderRoutes.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
-function renderRoutes({routes}) {
+function RenderRoutes({routes}) {
   return (
       <Switch>
         {routes.map((route, index) => {
@@ -14,4 +14,4 @@ function renderRoutes({routes}) {
   );
 }
 
-export default renderRoutes;
+export default RenderRoutes;
